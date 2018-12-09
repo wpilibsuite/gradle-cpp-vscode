8ed3e51706d45cfae193f278118bc8de88ae3386
Thad House <thadhouse1@gmail.com>
2018-12-09 15:50:49 -0800

    Update to gradle 5.0, publish with new plugin publish version
    Also publish on appveyor with new api account

--- src/main/java/edu/wpi/first/vscode/VsCodeConfigurationTask.java
+++ src/main/java/edu/wpi/first/vscode/VsCodeConfigurationTask.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Set;
 
+import javax.inject.Inject;
+
 import com.google.gson.GsonBuilder;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
@@ -18,7 +21,12 @@ import edu.wpi.first.vscode.tooling.models.ToolChains;
 
 public class VsCodeConfigurationTask extends DefaultTask {
   @OutputFile
-  public RegularFileProperty configFile = newOutputFile();
+  public RegularFileProperty configFile;
+
+  @Inject
+  public VsCodeConfigurationTask(ObjectFactory factory) {
+    configFile = factory.fileProperty();
+  }
 
   @TaskAction
   public void generate() {
